9e0d1381571fdcce331224bdf2827f88fa29fcf0
Jiu-xiao <2592509183@qq.com>
2024-04-11 04:16:24 +0800

    Add test for thread and queue.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include "libxr.hpp"
 #include "libxr_def.hpp"
 #include "libxr_time.hpp"
+#include "queue.hpp"
+#include "semaphore.hpp"
+#include "signal.hpp"
+#include "thread.hpp"
 #include <stdio.h>
 
 const char *TEST_NAME = NULL;
 
-#define TEST_STEP(_arg) TEST_NAME = _arg
+#define TEST_STEP(_arg)                                                        \
+  if (TEST_NAME)                                                               \
+    printf("Test [%s] Passed.\n", TEST_NAME);                                  \
+  TEST_NAME = _arg
 
 int main() {
   LibXR::PlatformInit();
@@ -41,6 +48,66 @@ int main() {
   LibXR::TimestampUS t3(1000), t4(2005);
   ASSERT(t4 - t3 == 1005);
   ASSERT(fabs((t4 - t3).to_secondf() - 0.001005) < 0.0000001);
+
+  TEST_STEP("Thread Test");
+
+  LibXR::Thread thread;
+
+  LibXR::Semaphore sem(0);
+
+  void (*thread_fun)(LibXR::Semaphore *) = [](LibXR::Semaphore *sem) {
+    LibXR::Signal::Wait(5);
+    sem->Post();
+    return;
+  };
+
+  thread.Create<LibXR::Semaphore *>(&sem, thread_fun, "test_task", 512,
+                                    LibXR::Thread::PRIORITY_REALTIME);
+
+  LibXR::Thread::Sleep(100);
+
+  LibXR::Signal::Action(thread, 5);
+
+  ASSERT(sem.Wait(100) == NO_ERR);
+
+  TEST_STEP("Queue Test");
+  auto queue = LibXR::Queue<float, 3>();
+
+  void (*queue_test_fun)(LibXR::Queue<float, 3> *) =
+      [](LibXR::Queue<float, 3> *queue) {
+        LibXR::Thread::Sleep(100);
+        queue->Push(1.2f);
+        LibXR::Thread::Sleep(10);
+        queue->Push(3.8f);
+        LibXR::Thread::Sleep(10);
+        queue->Push(100.f);
+        LibXR::Thread::Sleep(10);
+        queue->Push(0.0f);
+        LibXR::Thread::Sleep(10);
+        queue->Push(2.1f);
+        return;
+      };
+
+  thread.Create<LibXR::Queue<float, 3> *>(&queue, queue_test_fun, "test_task",
+                                          512,
+                                          LibXR::Thread::PRIORITY_REALTIME);
+  float tmp = 0.0f;
+
+  queue.Pop(tmp, 200);
+  ASSERT(tmp == 1.2f);
+  queue.Pop(tmp, 20);
+  ASSERT(tmp == 3.8f);
+  queue.Pop(tmp, 20);
+  ASSERT(tmp == 100.f);
+  queue.Pop(tmp, 20);
+  ASSERT(tmp == 0.0f);
+  queue.Pop(tmp, 20);
+  ASSERT(tmp == 2.1f);
+
+  queue.Pop(tmp, 20);
+  ASSERT(tmp == 2.1f);
+
+  TEST_STEP("End");
 
   return 0;
 }
