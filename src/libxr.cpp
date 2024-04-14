5c57e7887e949fc64d93da0edba8fffee557a526
Jiu-xiao <2592509183@qq.com>
2024-04-15 02:14:43 +0800

    Add timer/queue/list/rbt/crc.

--- src/libxr.cpp
+++ src/libxr.cpp
@@ -1,9 +1,26 @@
 #include "libxr.hpp"
+#include "crc.hpp"
 #include "libxr_def.hpp"
+#include "libxr_platform.hpp"
+#include "libxr_time.hpp"
 #include "libxr_type.hpp"
+#include "list.hpp"
+#include "system/timer.hpp"
+#include "thread.hpp"
+#include "timer.hpp"
 
 LibXR::Callback<void, const char *, uint32_t>
     LibXR::Assert::libxr_fatal_error_callback;
 
 LibXR::ReadFunction LibXR::STDIO::read;
 LibXR::WriteFunction LibXR::STDIO::write;
+LibXR::List<LibXR::Timer::ControlBlock>
+    LibXR::Timer::list_[LibXR::Thread::PRIORITY_NUMBER];
+uint8_t LibXR::CRC8::tab[256];
+bool LibXR::CRC8::inited = false;
+uint16_t LibXR::CRC16::tab[256];
+bool LibXR::CRC16::inited = false;
+uint32_t LibXR::CRC32::tab[256];
+bool LibXR::CRC32::inited = false;
+
+void LibXR::LibXR_Init() { PlatformInit(); }
---
+++ src/system/timer.hpp
@@ -0,0 +1,106 @@
+#pragma once
+
+#include "libxr.hpp"
+#include "libxr_def.hpp"
+#include "list.hpp"
+#include "thread.hpp"
+
+namespace LibXR {
+class Timer {
+public:
+  class ControlBlock {
+  public:
+    void Run() { fun_(handle); }
+
+    void (*fun_)(void *);
+    void *handle;
+    uint32_t cycle_;
+    uint32_t count_;
+    bool enable_;
+    Thread::Priority priority;
+  };
+
+  typedef LibXR::List<ControlBlock>::Node *TimerHandle;
+
+  template <typename ArgType>
+  static TimerHandle CreatetTask(void (*fun)(ArgType), ArgType arg,
+                                 uint32_t cycle, Thread::Priority priority) {
+    ASSERT(priority < Thread::PRIORITY_NUMBER);
+    ASSERT(cycle > 0);
+
+    typedef struct {
+      LibXR::List<ControlBlock>::Node ctrl_block;
+      ArgType arg;
+      void (*fun)(ArgType);
+    } Data;
+
+    Data *data = new Data;
+    data->fun = fun;
+    data->arg = arg;
+
+    data->ctrl_block.data_.handle = data;
+    data->ctrl_block.data_.fun_ = [](void *arg) {
+      Data *data = reinterpret_cast<Data *>(arg);
+      data->fun(data->arg);
+    };
+    data->ctrl_block.data_.count_ = 0;
+    data->ctrl_block.data_.cycle_ = cycle;
+    data->ctrl_block.data_.enable_ = false;
+    data->ctrl_block.data_.priority = priority;
+
+    return &data->ctrl_block;
+  }
+
+  static void Start(TimerHandle handle) { handle->data_.enable_ = true; }
+
+  static void Stop(TimerHandle handle) { handle->data_.enable_ = false; }
+
+  static void SetCycle(TimerHandle handle, uint32_t cycle) {
+    handle->data_.cycle_ = cycle;
+  }
+
+  static void SetPriority(TimerHandle handle, Thread::Priority priority) {
+    ASSERT(priority < Thread::PRIORITY_NUMBER);
+
+    if (handle->next_) {
+      Remove(handle);
+      handle->data_.priority = priority;
+      Add(handle);
+    } else {
+      handle->data_.priority = priority;
+    }
+  }
+
+  static void Remove(TimerHandle handle) {
+    ASSERT(handle->next_);
+    list_[handle->data_.priority].Delete(*handle);
+  }
+
+  static void Add(TimerHandle handle) {
+    ASSERT(!handle->next_);
+    list_[handle->data_.priority].Add(*handle);
+  }
+
+  static void Refresh(Thread::Priority priority) {
+    auto fun = [](ControlBlock &block, void *) {
+      if (!block.enable_) {
+        return NO_ERR;
+      }
+
+      block.count_++;
+
+      if (block.count_ >= block.cycle_) {
+        block.count_ = 0;
+        block.Run();
+      }
+
+      return NO_ERR;
+    };
+
+    list_[priority].Foreach<void *>(fun, (void *)0);
+  }
+
+  static LibXR::List<ControlBlock> list_[Thread::PRIORITY_NUMBER];
+};
+
+} // namespace LibXR
