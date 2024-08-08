028a49f9970512b576493cb56f68543e8275144b
Jiu-xiao <2592509183@qq.com>
2024-08-08 19:09:10 +0800

    Add support for async.

--- src/system/async.hpp
+++ src/system/async.hpp
@@ -4,9 +4,51 @@
 #include "list.hpp"
 #include "mutex.hpp"
 #include "rbt.hpp"
+#include "semaphore.hpp"
+#include "thread.hpp"
 
 namespace LibXR {
 class ASync {
 public:
+  enum class Status {
+    REDAY,
+    BUSY,
+    DONE,
+  };
+
+  ASync(size_t stack_depth, Thread::Priority priority);
+
+  static void thread_fun(ASync *async) {
+    while (true) {
+      if (async->sem_.Wait() == ErrorCode::OK) {
+        async->job_.Run(false, async);
+        async->status_ = Status::DONE;
+      }
+    }
+  }
+
+  Status status_ = Status::REDAY;
+
+  Status GetStatus() {
+    if (status_ != Status::DONE) {
+      return status_;
+    } else {
+      status_ = Status::REDAY;
+      return Status::DONE;
+    }
+  }
+
+  ErrorCode AssignJob(Callback<ASync *> job);
+
+  void AssignJobFromCallback(Callback<ASync *> job, bool in_isr) {
+    job_ = job;
+    status_ = Status::BUSY;
+    sem_.PostFromCallback(in_isr);
+  }
+
+  Callback<ASync *> job_;
+  Semaphore sem_;
+
+  Thread thread_handle_;
 };
 } // namespace LibXR
