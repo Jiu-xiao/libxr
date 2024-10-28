4d7810769619e3ae707ee99f66401e5b1a997ba6
Jiu-xiao <2592509183@qq.com>
2024-10-29 03:56:48 +0800

    Fix build for None and Webots.

--- system/Webots/libxr_system.cpp
+++ system/Webots/libxr_system.cpp
@@ -28,10 +28,9 @@ static float time_step = 0.0f;
 LibXR::ConditionVar *_libxr_webots_time_notify = NULL;
 
 void LibXR::PlatformInit() {
-  auto write_fun = [](WriteOperation &op, ConstRawData data, WritePort &port) {
-    auto ans = fwrite(data.addr_, 1, data.size_, stdout);
+  auto write_fun = [](WritePort &port) {
+    auto ans = fwrite(port.info_.data.addr_, 1, port.info_.data.size_, stdout);
 
-    UNUSED(op);
     UNUSED(ans);
 
     port.UpdateStatus(false, ErrorCode::OK);
@@ -43,14 +42,13 @@ void LibXR::PlatformInit() {
 
   *LibXR::STDIO::write = write_fun;
 
-  auto read_fun = [](Operation<ErrorCode, RawData &> &op, RawData buff,
-                     ReadPort &port) {
-    auto need_read = buff.size_;
+  auto read_fun = [](ReadPort &port) {
+    auto need_read = port.info_.data.size_;
 
-    UNUSED(op);
     UNUSED(need_read);
 
-    buff.size_ = fread(buff.addr_, sizeof(char), buff.size_, stdin);
+    port.info_.data.size_ = fread(port.info_.data.addr_, sizeof(char),
+                                  port.info_.data.size_, stdin);
 
     port.UpdateStatus(false, ErrorCode::OK);
     return ErrorCode::OK;
