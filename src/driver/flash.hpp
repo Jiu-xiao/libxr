d853edf323f5ee170bbd264e507a4b325ec46407
Jiu-xiao <2592509183@qq.com>
2025-03-13 07:46:12 +0800

    Add DatabaseRaw.

--- src/driver/flash.hpp
+++ src/driver/flash.hpp
@@ -8,10 +8,13 @@ namespace LibXR {
 
 class Flash {
  public:
-  Flash(size_t min_erase_size, RawData flash_area)
-      : min_erase_size_(min_erase_size), flash_area_(flash_area) {}
+  Flash(size_t min_erase_size, size_t min_write_size, RawData flash_area)
+      : min_erase_size_(min_erase_size),
+        min_write_size_(min_write_size),
+        flash_area_(flash_area) {}
 
   size_t min_erase_size_ = 0;
+  size_t min_write_size_ = 0;
   RawData flash_area_;
 
   virtual ErrorCode Erase(size_t offset, size_t size) = 0;
