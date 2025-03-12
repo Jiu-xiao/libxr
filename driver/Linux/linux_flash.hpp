06843ca780652a971531bc753f51838542ee2b5c
Jiu-xiao <2592509183@qq.com>
2025-03-13 07:46:12 +0800

    Add DatabaseRaw.

--- driver/Linux/linux_flash.hpp
+++ driver/Linux/linux_flash.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
 #include <array>
+#include <cstddef>
 #include <cstdint>
 #include <cstring>
 #include <fstream>
 
 #include "flash.hpp"
 #include "libxr.hpp"
+#include "libxr_def.hpp"
 #include "libxr_type.hpp"
 
 namespace LibXR {
@@ -14,9 +16,16 @@ namespace LibXR {
 template <size_t FLASH_SIZE>
 class LinuxBinaryFileFlash : public Flash {
  public:
-  LinuxBinaryFileFlash(size_t min_erase_size, const std::string& file_path)
-      : Flash(min_erase_size, RawData(flash_area_.data(), flash_area_.size())),
-        file_path_(file_path) {
+  LinuxBinaryFileFlash(const std::string& file_path,
+                       size_t min_erase_size = FLASH_SIZE / 2,
+                       size_t min_write_size = sizeof(uint8_t),
+                       bool write_order_check = false,
+                       bool write_as_one_check = false)
+      : Flash(min_erase_size, min_write_size,
+              RawData(flash_area_.data(), flash_area_.size())),
+        file_path_(file_path),
+        write_order_check_(write_order_check),
+        write_as_one_check_(write_as_one_check) {
     std::ifstream file(file_path_, std::ios::binary);
     if (file) {
       file.read(reinterpret_cast<char*>(flash_area_.data()),
@@ -25,13 +34,15 @@ class LinuxBinaryFileFlash : public Flash {
   }
 
   ErrorCode Erase(size_t offset, size_t size) override {
-    if ((offset % min_erase_size_) != 0 || (size % min_erase_size_) != 0) {
-      return ErrorCode::ARG_ERR;
-    }
+    ASSERT(offset % min_erase_size_ == 0);
+    ASSERT(size % min_erase_size_ == 0);
+
     if ((offset + size) > flash_area_.size()) {
       return ErrorCode::OUT_OF_RANGE;
     }
+
     std::memset(flash_area_.data() + offset, 0xFF, size);
+
     return SyncToFile();
   }
 
@@ -39,13 +50,28 @@ class LinuxBinaryFileFlash : public Flash {
     if ((offset + data.size_) > flash_area_.size()) {
       return ErrorCode::OUT_OF_RANGE;
     }
+
+    if (offset % min_write_size_ != 0 || data.size_ % min_write_size_ != 0) {
+      ASSERT(false);
+      return ErrorCode::FAILED;
+    }
+
+    if (write_order_check_) {
+      ASSERT(offset % min_erase_size_ == 0);
+    }
+
     uint8_t* dst = flash_area_.data() + offset;
     const uint8_t* src = static_cast<const uint8_t*>(data.addr_);
-    for (size_t i = 0; i < data.size_; ++i) {
-      if ((dst[i] & src[i]) != src[i]) {
-        return ErrorCode::FAILED;
+
+    if (write_as_one_check_) {
+      for (size_t i = 0; i < data.size_; ++i) {
+        if ((~dst[i] & src[i])) {
+          ASSERT(false);
+          return ErrorCode::FAILED;
+        }
       }
     }
+
     std::memcpy(dst, src, data.size_);
     return SyncToFile();
   }
@@ -53,6 +79,8 @@ class LinuxBinaryFileFlash : public Flash {
  private:
   std::string file_path_;
   std::array<uint8_t, FLASH_SIZE> flash_area_ = {};
+  bool write_order_check_;
+  bool write_as_one_check_;
 
   ErrorCode SyncToFile() {
     std::ofstream file(file_path_, std::ios::binary | std::ios::trunc);
--- test/test_database.cpp
+++ test/test_database.cpp
@@ -1,32 +1,33 @@
 #include <sys/types.h>
 
 #include <cstdint>
+#include <cstdlib>
 #include <cstring>
 
 #include "database.hpp"
+#include "libxr_def.hpp"
 #include "linux_flash.hpp"
 #include "test.hpp"
 
 using namespace LibXR;
 
 void test_database() {
-  constexpr size_t FLASH_SIZE = 1024;
-  LinuxBinaryFileFlash<FLASH_SIZE> flash(16, "/tmp/flash_test.bin");
+  constexpr size_t FLASH_SIZE = 2048;
+  LinuxBinaryFileFlash<FLASH_SIZE> flash("/tmp/flash_test.bin", 512, 4, true,
+                                         true);
   DatabaseRawSequential db(flash);
-
-  db.Init();
 
   std::array<uint32_t, 1> key1_data = {1};
   std::array<uint32_t, 2> key2_data = {11, 22};
   std::array<uint32_t, 3> key3_data = {111, 222, 333};
   std::array<uint32_t, 4> key4_data = {1111, 2222, 3333, 4444};
 
-  key4_data[1] = 123456;
-
   DatabaseRawSequential::Key key1(db, "key1", key1_data);
   DatabaseRawSequential::Key key2(db, "keasdasy2", key2_data);
   DatabaseRawSequential::Key key3(db, "keaasdasdy3", key3_data);
   DatabaseRawSequential::Key key4(db, "keyaskdhasjh4", key4_data);
+
+  key4_data[1] = 123456;
 
   key4 = key4_data;
 
@@ -41,4 +42,58 @@ void test_database() {
   ASSERT(memcmp(&key2_data[0], &key2.data_[0], sizeof(key2_data)) == 0);
   ASSERT(memcmp(&key3_data[0], &key3.data_[0], sizeof(key3_data)) == 0);
   ASSERT(memcmp(&key4_data[0], &key4.data_[0], sizeof(key4_data)) == 0);
+
+  LinuxBinaryFileFlash<FLASH_SIZE> flash_2("/tmp/flash_test_2.bin", 512, 16,
+                                           false, true);
+  DatabaseRaw<16> db_2(flash_2);
+
+  DatabaseRawSequential::Key key1_2(db_2, "key1", key1_data);
+  DatabaseRawSequential::Key key2_2(db_2, "keasdasy2", key2_data);
+  DatabaseRawSequential::Key key3_2(db_2, "keaasdasdy3", key3_data);
+  DatabaseRawSequential::Key key4_2(db_2, "keyaskdhasjh4", key4_data);
+
+  key4_data[1] = 1234567;
+
+  key1_2 = key1_data;
+  key2_2 = key2_data;
+  key3_2 = key3_data;
+  key4_2 = key4_data;
+
+  key1_2.Load();
+  key2_2.Load();
+  key3_2.Load();
+  key4_2.Load();
+
+  ASSERT(memcmp(&key1_data[0], &key1_2.data_[0], sizeof(key1_data)) == 0);
+  ASSERT(memcmp(&key2_data[0], &key2_2.data_[0], sizeof(key2_data)) == 0);
+  ASSERT(memcmp(&key3_data[0], &key3_2.data_[0], sizeof(key3_data)) == 0);
+  ASSERT(memcmp(&key4_data[0], &key4_2.data_[0], sizeof(key4_data)) == 0);
+
+  for (size_t i = 0; i < FLASH_SIZE; i++) {
+    for (int j = 0; j < Thread::GetTime() % 3; j++) {
+      key1_data[0] = j;
+      key1_2 = key1_data;
+    }
+    for (int j = 0; j < Thread::GetTime() % 3; j++) {
+      key2_data[0] = j;
+      key2_2 = key2_data;
+    }
+    for (int j = 0; j < Thread::GetTime() % 3; j++) {
+      key3_data[0] = j;
+      key3_2 = key3_data;
+    }
+    for (int j = 0; j < Thread::GetTime() % 3; j++) {
+      key4_data[0] = j;
+      key4_2 = key4_data;
+    }
+
+    key1_2.Load();
+    key2_2.Load();
+    key3_2.Load();
+    key4_2.Load();
+    ASSERT(memcmp(&key1_data[0], &key1_2.data_[0], sizeof(key1_data)) == 0);
+    ASSERT(memcmp(&key2_data[0], &key2_2.data_[0], sizeof(key2_data)) == 0);
+    ASSERT(memcmp(&key3_data[0], &key3_2.data_[0], sizeof(key3_data)) == 0);
+    ASSERT(memcmp(&key4_data[0], &key4_2.data_[0], sizeof(key4_data)) == 0);
+  }
 }
