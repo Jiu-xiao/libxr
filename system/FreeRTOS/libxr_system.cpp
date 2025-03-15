728430849e05587c8dd2ca71f0db9286ba09f259
Jiu-xiao <2592509183@qq.com>
2025-03-15 09:13:18 +0800

    remove TIMER_PRI and TIMER_STACK_DEPTH.

--- system/FreeRTOS/libxr_system.cpp
+++ system/FreeRTOS/libxr_system.cpp
@@ -1,10 +1,14 @@
 #include "libxr.hpp"
+#include "timer.hpp"
 
-void LibXR::PlatformInit() {
+void LibXR::PlatformInit(uint32_t timer_pri, uint32_t timer_stack_depth) {
   if (Timebase::timebase == nullptr) {
     /* You should initialize Timebase first */
     ASSERT(false);
   }
+
+  LibXR::Timer::priority = static_cast<LibXR::Thread::Priority>(timer_pri);
+  LibXR::Timer::stack_depth = timer_stack_depth;
 
   uint32_t time_need_to_catch_up =
       Timebase::GetMilliseconds() - xTaskGetTickCount();
