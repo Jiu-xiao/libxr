9e80e9eb54033cbdba1632c2ade67a0569b66018
jiu-xiao <2592509183@qq.com>
2025-03-29 03:22:07 +0000

    STM32GPIO support STM32G0/F0/L0

--- driver/st/stm32_gpio.cpp
+++ driver/st/stm32_gpio.cpp
@@ -6,26 +6,56 @@ using namespace LibXR;
 
 STM32GPIO *STM32GPIO::map[STM32_GPIO_EXTI_NUMBER] = {nullptr};
 
-stm32_gpio_exti_t STM32_GPIO_EXTI_GetID(uint16_t pin) {  // NOLINT
-  pin = __builtin_ctz(pin);
-  if (pin < 5) {
-    return static_cast<stm32_gpio_exti_t>(pin);
-  } else if (pin < 10) {
-    return STM32_GPIO_EXTI_5_9;
-  } else if (pin < 16) {
-    return STM32_GPIO_EXTI_10_15;
-  } else {
+stm32_gpio_exti_t STM32_GPIO_EXTI_GetID(uint16_t pin)
+{  // NOLINT
+  uint8_t pin_num = __builtin_ctz(pin);
+
+  if (pin == 0 || pin_num > 15)
+  {
     ASSERT(false);
+    return STM32_GPIO_EXTI_NUMBER;
   }
+
+#if defined(STM32F0) || defined(STM32G0) || defined(STM32L0)
+  if (pin_num <= 1)
+  {
+    return STM32_GPIO_EXTI_0_1;
+  }
+  else if (pin_num <= 3)
+  {
+    return STM32_GPIO_EXTI_2_3;
+  }
+  else
+  {
+    return STM32_GPIO_EXTI_4_15;
+  }
+
+#else
+  if (pin_num <= 4)
+  {
+    return static_cast<stm32_gpio_exti_t>(pin_num);
+  }
+  else if (pin_num <= 9)
+  {
+    return STM32_GPIO_EXTI_5_9;
+  }
+  else
+  {
+    return STM32_GPIO_EXTI_10_15;
+  }
+
+#endif
 
   return STM32_GPIO_EXTI_NUMBER;
 }
 
-extern "C" void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
+extern "C" void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
   auto id = STM32_GPIO_EXTI_GetID(GPIO_Pin);
   auto gpio = STM32GPIO::map[id];
 
-  if (gpio) {
+  if (gpio)
+  {
     gpio->callback_.Run(true);
   }
 }
