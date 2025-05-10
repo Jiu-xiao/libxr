8619616636eb84a7cdfa31b5abb3c210d095406f
jiu-xiao <2592509183@qq.com>
2025-05-10 21:25:03 +0100

    Faster logger.

--- src/middleware/logger.cpp
+++ src/middleware/logger.cpp
@@ -15,10 +15,31 @@ static Topic log_topic;  ///< 日志发布主题 / Log publish topic
 void Logger::Init()
 {
   log_topic = Topic::CreateTopic<LogData>("/xr/log", nullptr, false, true);
+
+#if LIBXR_PRINTF_BUFFER_SIZE > 0
+  void (*log_cb_fun)(bool in_isr, Topic, RawData &log_data) =
+      [](bool, Topic tp, LibXR::RawData &log_data)
+  {
+    UNUSED(tp);
+
+    auto log = reinterpret_cast<LogData *>(log_data.addr_);
+
+    if (LIBXR_LOG_OUTPUT_LEVEL >= static_cast<uint8_t>(log->level) &&
+        STDIO::write_->Writable())
+    {
+      PrintToTerminal(*log);
+    }
+  };
+
+  auto log_cb = LibXR::Callback<LibXR::RawData &>::Create(log_cb_fun, log_topic);
+  log_topic.RegisterCallback(log_cb);
+#endif
+
+  initialized_ = true;
 }
 
 // NOLINTNEXTLINE
-void Logger::Publish(LogLevel level, const char* file, uint32_t line, const char* fmt,
+void Logger::Publish(LogLevel level, const char *file, uint32_t line, const char *fmt,
                      ...)
 {
   if (!initialized_)
@@ -38,11 +59,4 @@ void Logger::Publish(LogLevel level, const char* file, uint32_t line, const char
   va_end(args);
 
   log_topic.Publish(data);
-
-#if LIBXR_PRINTF_BUFFER_SIZE > 0
-  if (LIBXR_LOG_OUTPUT_LEVEL >= static_cast<uint8_t>(level) && STDIO::write_->Writable())
-  {
-    PrintToTerminal(data);
-  }
-#endif
 }
