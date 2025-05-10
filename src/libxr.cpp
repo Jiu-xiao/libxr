eee20b2ab41a03c6d27c558dccdc3b10e620e82e
Jiu-xiao <2592509183@qq.com>
2025-05-10 05:47:15 +0100

    Add delay to libxr_fatal_error.

--- src/libxr.cpp
+++ src/libxr.cpp
@@ -1,5 +1,7 @@
 #include "libxr.hpp"
 
+#include "thread.hpp"
+
 void libxr_fatal_error(const char *file, uint32_t line, bool in_isr)
 {
   volatile bool stop = false;
@@ -14,5 +16,7 @@ void libxr_fatal_error(const char *file, uint32_t line, bool in_isr)
     {
       LibXR::Assert::libxr_fatal_error_callback_->Run(in_isr, file, line);
     }
+
+    LibXR::Thread::Sleep(500);
   }
 }
