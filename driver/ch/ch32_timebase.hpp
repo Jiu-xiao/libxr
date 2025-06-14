29a835aa808da1fcbc8d22ef4ed7016aa54eac25
jiu-xiao <2592509183@qq.com>
2025-06-14 11:38:37 +0100

    driver/ch32: - 新增 ch32_timebase.hpp，实现基于 CH32 的微秒/毫秒时基 - CMakeLists.txt 自动收集 CH32 目录下 C/C++ 驱动源码并纳入构建
    driver/tinyusb:
    - 新增 tinyusb_virtual_uart.hpp 与 tinyusb_virtual_uart.cpp
    - 实现基于 TinyUSB 的虚拟串口 UART，支持 Doxygen 注释
    - 支持自定义包长、可查询连接状态、读写端口等

---
+++ driver/ch/ch32_timebase.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "core_riscv.h"
+
+extern uint32_t SystemCoreClock;
+
+namespace LibXR
+{
+
+class CH32Timebase : public Timebase
+{
+ public:
+  CH32Timebase()
+      : Timebase(UINT64_MAX, UINT32_MAX)
+  {
+    cnt_per_microsec_ = SystemCoreClock / 1000000;
+  }
+
+  TimestampUS _get_microseconds() override
+  {
+    uint64_t cnt = SysTick->CNT;
+    return TimestampUS(cnt / cnt_per_microsec_);
+  }
+
+  TimestampMS _get_milliseconds() override { return cnt_per_microsec_ / 1000; }
+
+  static inline uint32_t cnt_per_microsec_;
+};
+
+}  // namespace LibXR
