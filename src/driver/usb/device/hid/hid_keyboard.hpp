34815727ed007f89caa8d5142561a2d5ddb14eb3
jiu-xiao <2592509183@qq.com>
2025-07-26 17:46:01 +0100

    Add hid_mouse.

--- src/driver/usb/device/hid/hid_keyboard.hpp
+++ src/driver/usb/device/hid/hid_keyboard.hpp
@@ -72,7 +72,7 @@ class HIDKeyboard : public HID<sizeof(HID_KEYBOARD_REPORT_DESC), 8, 1>
   /**
    * @brief 修饰键枚举 Modifier enum
    */
-  enum class Modifier : uint8_t
+  enum Modifier : uint8_t
   {
     NONE = 0x00,         ///< 无修饰键 / No modifier
     LEFT_CTRL = 0x01,    ///< 左 Ctrl / Left Control
@@ -361,7 +361,7 @@ class HIDKeyboard : public HID<sizeof(HID_KEYBOARD_REPORT_DESC), 8, 1>
    * @param mods 修饰键 / Modifiers
    * @note 标准 HID Boot 协议只支持最多 6 键 / Boot protocol supports up to 6 keys
    */
-  void PressKey(std::initializer_list<KeyCode> keys, Modifier mods = Modifier::NONE)
+  void PressKey(std::initializer_list<KeyCode> keys, uint8_t mods = Modifier::NONE)
   {
     ASSERT(keys.size() <= 6);
     report_ = {static_cast<uint8_t>(mods), 0, {0}};
@@ -452,17 +452,6 @@ class HIDKeyboard : public HID<sizeof(HID_KEYBOARD_REPORT_DESC), 8, 1>
   }
 
  public:
-  /**
-   * @brief 修饰键位支持按位或 / Support bitwise OR for Modifier
-   * @param a 第一个修饰键 / First modifier
-   * @param b 第二个修饰键 / Second modifier
-   * @return Modifier 合成修饰键 / Combined modifier
-   */
-  friend inline Modifier operator|(Modifier a, Modifier b)
-  {
-    return static_cast<Modifier>(static_cast<uint8_t>(a) | static_cast<uint8_t>(b));
-  }
-
   /**
    * @brief 获取 NumLock 状态 / Get NumLock status
    * @return true 开启 / Enabled, false 关闭 / Disabled
