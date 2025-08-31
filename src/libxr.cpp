e5730a9226d1a6cd90fac25611c2d6eaa40a4f88
jiu-xiao <2592509183@qq.com>
2025-08-31 16:55:50 +0100

    fix: improve WritePort locking and fatal error handling

--- src/libxr.cpp
+++ src/libxr.cpp
@@ -7,16 +7,23 @@ void libxr_fatal_error(const char *file, uint32_t line, bool in_isr)
   volatile bool stop = false;
   while (!stop)
   {
-    if (LibXR::STDIO::write_ && LibXR::STDIO::write_->Writable())
+    if (in_isr)
     {
-      printf("Fatal error at %s:%d\r\n", file, static_cast<int>(line));
+      *(volatile int *)0 = 0;  // NOLINT
     }
-
-    if (!LibXR::Assert::libxr_fatal_error_callback_.Empty())
+    else
     {
-      LibXR::Assert::libxr_fatal_error_callback_.Run(in_isr, file, line);
-    }
+      if (LibXR::STDIO::write_ && LibXR::STDIO::write_->Writable())
+      {
+        LibXR::STDIO::Printf("Fatal error at %s:%d\r\n", file, static_cast<int>(line));
+      }
 
-    LibXR::Thread::Sleep(500);
+      if (!LibXR::Assert::libxr_fatal_error_callback_.Empty())
+      {
+        LibXR::Assert::libxr_fatal_error_callback_.Run(in_isr, file, line);
+      }
+
+      LibXR::Thread::Sleep(500);
+    }
   }
 }
