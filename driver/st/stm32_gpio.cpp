88fa62df23aa235091f323f33aa0ef14adb12f49
Jiu-xiao <2592509183@qq.com>
2025-12-28 13:28:52 +0800

    st/gpio: route EXTI callbacks by line (0..15), drop group-based mapping

--- driver/st/stm32_gpio.cpp
+++ driver/st/stm32_gpio.cpp
@@ -4,55 +4,14 @@
 
 using namespace LibXR;
 
-STM32GPIO* STM32GPIO::map[STM32_GPIO_EXTI_NUMBER] = {nullptr};
+STM32GPIO* STM32GPIO::map[16] = {nullptr};
 
-/**
- * @brief 获取 GPIO 扩展中断号
- *
- * @param pin 引脚
- * @return stm32_gpio_exti_t 中断号 Interrupt number
- */
-stm32_gpio_exti_t STM32_GPIO_EXTI_GetID(uint16_t pin)
-{  // NOLINT
-  uint8_t pin_num = __builtin_ctz(pin);
-
-  if (pin == 0 || pin_num > 15)
-  {
-    ASSERT(false);
-    return STM32_GPIO_EXTI_NUMBER;
-  }
-
-#if defined(STM32F0) || defined(STM32G0) || defined(STM32L0)
-  if (pin_num <= 1)
-  {
-    return STM32_GPIO_EXTI_0_1;
-  }
-  else if (pin_num <= 3)
-  {
-    return STM32_GPIO_EXTI_2_3;
-  }
-  else
-  {
-    return STM32_GPIO_EXTI_4_15;
-  }
-
-#else
-  if (pin_num <= 4)
-  {
-    return static_cast<stm32_gpio_exti_t>(pin_num);
-  }
-  else if (pin_num <= 9)
-  {
-    return STM32_GPIO_EXTI_5_9;
-  }
-  else
-  {
-    return STM32_GPIO_EXTI_10_15;
-  }
-
-#endif
-
-  return STM32_GPIO_EXTI_NUMBER;
+// NOLINTNEXTLINE
+static inline uint8_t STM32_GPIO_PinToLine(uint16_t pin)
+{
+  ASSERT(pin != 0 && (pin & (pin - 1)) == 0);
+  const uint8_t LINE = static_cast<uint8_t>(__builtin_ctz(static_cast<unsigned>(pin)));
+  return LINE;
 }
 
 STM32GPIO::STM32GPIO(GPIO_TypeDef* port, uint16_t pin, IRQn_Type irq)
@@ -60,7 +19,7 @@ STM32GPIO::STM32GPIO(GPIO_TypeDef* port, uint16_t pin, IRQn_Type irq)
 {
   if (irq_ != NonMaskableInt_IRQn)
   {
-    map[STM32_GPIO_EXTI_GetID(pin)] = this;
+    map[STM32_GPIO_PinToLine(pin)] = this;
   }
 }
 
@@ -138,10 +97,8 @@ ErrorCode STM32GPIO::SetConfig(Configuration config)
 
 extern "C" void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
-  auto id = STM32_GPIO_EXTI_GetID(GPIO_Pin);
-  auto gpio = STM32GPIO::map[id];
-
-  if (gpio)
+  const uint8_t LINE = STM32_GPIO_PinToLine(GPIO_Pin);
+  if (auto* gpio = STM32GPIO::map[LINE])
   {
     gpio->callback_.Run(true);
   }
