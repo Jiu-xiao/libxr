3f3e5c69d8e4043f2b8e57fc1ea01d149ccda422
molqzone <toiperus@sohu.com>
2026-02-15 13:49:55 +0800

    style(mspm0): format atomic shim to satisfy clang-format checks

--- driver/mspm0/mspm0_atomic_shim.c
+++ driver/mspm0/mspm0_atomic_shim.c
@@ -1,15 +1,18 @@
-#include "msp.h" 
+#include "msp.h"
 
-static inline uint32_t atomic_enter(void) {
-    uint32_t primask_state = __get_PRIMASK();
-    __disable_irq();
-    return primask_state;
+static inline uint32_t atomic_enter(void)
+{
+  uint32_t primask_state = __get_PRIMASK();
+  __disable_irq();
+  return primask_state;
 }
 
-static inline void atomic_exit(uint32_t primask_state) {
-    if (!primask_state) {
-        __enable_irq();
-    }
+static inline void atomic_exit(uint32_t primask_state)
+{
+  if (!primask_state)
+  {
+    __enable_irq();
+  }
 }
 
 #define UNUSED(x) (void)(x)
@@ -28,21 +31,23 @@ static inline void atomic_exit(uint32_t primask_state) {
  * (ignored)
  * @retval 返回 1 表示成功，0 表示失败 / Returns 1 on success, 0 on failure
  */
-__attribute__((weak)) _Bool
-__atomic_compare_exchange_4(volatile void *ptr, void *expected, unsigned int desired, // NOLINT
-                            _Bool weak, int success_memorder, int failure_memorder)
+__attribute__((weak)) _Bool __atomic_compare_exchange_4(volatile void* ptr,
+                                                        void* expected,
+                                                        unsigned int desired,  // NOLINT
+                                                        _Bool weak, int success_memorder,
+                                                        int failure_memorder)
 {
   UNUSED(weak);
   UNUSED(success_memorder);
   UNUSED(failure_memorder);
 
-  volatile unsigned int *addr = (volatile unsigned int *)ptr;
-  unsigned int expected_val = *(unsigned int *)expected;
+  volatile unsigned int* addr = (volatile unsigned int*)ptr;
+  unsigned int expected_val = *(unsigned int*)expected;
   _Bool result = false;
 
   // 2. 使用安全的方式开关中断
-  uint32_t primask = atomic_enter(); 
-  
+  uint32_t primask = atomic_enter();
+
   if (*addr == expected_val)
   {
     *addr = desired;
@@ -50,11 +55,11 @@ __atomic_compare_exchange_4(volatile void *ptr, void *expected, unsigned int des
   }
   else
   {
-    *(unsigned int *)expected = *addr;
+    *(unsigned int*)expected = *addr;
     result = false;
   }
-  
-  atomic_exit(primask); // 恢复状态
+
+  atomic_exit(primask);  // 恢复状态
   return result;
 }
 
@@ -65,11 +70,11 @@ __atomic_compare_exchange_4(volatile void *ptr, void *expected, unsigned int des
  * @param  memorder 内存顺序标志 / Memory order (ignored)
  * @retval 无返回值 / None
  */
-__attribute__((weak)) void 
-__atomic_store_4(volatile void *ptr, unsigned int val, int memorder) // NOLINT
+__attribute__((weak)) void __atomic_store_4(volatile void* ptr, unsigned int val,
+                                            int memorder)  // NOLINT
 {
   UNUSED(memorder);
-  volatile unsigned int *addr = (volatile unsigned int *)ptr;
+  volatile unsigned int* addr = (volatile unsigned int*)ptr;
 
   uint32_t primask = atomic_enter();
   *addr = val;
@@ -82,11 +87,11 @@ __atomic_store_4(volatile void *ptr, unsigned int val, int memorder) // NOLINT
  * @param  memorder 内存顺序标志 / Memory order (ignored)
  * @retval 当前值 / Returns the current value
  */
-__attribute__((weak)) unsigned int 
-__atomic_load_4(const volatile void *ptr, int memorder) // NOLINT
+__attribute__((weak)) unsigned int __atomic_load_4(const volatile void* ptr,
+                                                   int memorder)  // NOLINT
 {
   UNUSED(memorder);
-  volatile unsigned int *addr = (volatile unsigned int *)ptr;
+  volatile unsigned int* addr = (volatile unsigned int*)ptr;
   unsigned int val = 0;
 
   uint32_t primask = atomic_enter();
@@ -102,11 +107,12 @@ __atomic_load_4(const volatile void *ptr, int memorder) // NOLINT
  * @param  memorder 内存顺序标志 / Memory order (ignored)
  * @retval 交换前的旧值 / Returns the old value before exchange
  */
-__attribute__((weak)) unsigned int 
-__atomic_exchange_4(volatile void *ptr, unsigned int val, int memorder) // NOLINT
+__attribute__((weak)) unsigned int __atomic_exchange_4(volatile void* ptr,
+                                                       unsigned int val,
+                                                       int memorder)  // NOLINT
 {
   UNUSED(memorder);
-  volatile unsigned int *addr = (volatile unsigned int *)ptr;
+  volatile unsigned int* addr = (volatile unsigned int*)ptr;
   unsigned int old_val = 0;
 
   uint32_t primask = atomic_enter();
@@ -123,11 +129,12 @@ __atomic_exchange_4(volatile void *ptr, unsigned int val, int memorder) // NOLIN
  * @param  memorder 内存顺序标志 / Memory order (ignored)
  * @retval 加法前的旧值 / Returns the old value before addition
  */
-__attribute__((weak)) unsigned int 
-__atomic_fetch_add_4(volatile void *ptr, unsigned int val, int memorder) // NOLINT
+__attribute__((weak)) unsigned int __atomic_fetch_add_4(volatile void* ptr,
+                                                        unsigned int val,
+                                                        int memorder)  // NOLINT
 {
   UNUSED(memorder);
-  volatile unsigned int *addr = (volatile unsigned int *)ptr;
+  volatile unsigned int* addr = (volatile unsigned int*)ptr;
   unsigned int old_val = 0;
 
   uint32_t primask = atomic_enter();
@@ -144,11 +151,12 @@ __atomic_fetch_add_4(volatile void *ptr, unsigned int val, int memorder) // NOLI
  * @param  memorder 内存顺序标志 / Memory order (ignored)
  * @retval 减法前的旧值 / Returns the old value before subtraction
  */
-__attribute__((weak)) unsigned int 
-__atomic_fetch_sub_4(volatile void *ptr, unsigned int val, int memorder) // NOLINT
+__attribute__((weak)) unsigned int __atomic_fetch_sub_4(volatile void* ptr,
+                                                        unsigned int val,
+                                                        int memorder)  // NOLINT
 {
   UNUSED(memorder);
-  volatile unsigned int *addr = (volatile unsigned int *)ptr;
+  volatile unsigned int* addr = (volatile unsigned int*)ptr;
   unsigned int old_val = 0;
 
   uint32_t primask = atomic_enter();
@@ -165,11 +173,12 @@ __atomic_fetch_sub_4(volatile void *ptr, unsigned int val, int memorder) // NOLI
  * @param  memorder 内存顺序标志（忽略） / Memory order (ignored)
  * @retval 交换前的旧值 / Returns the old value before exchange
  */
-__attribute__((weak)) unsigned char 
-__atomic_exchange_1(volatile void *ptr, unsigned char val, int memorder) // NOLINT
+__attribute__((weak)) unsigned char __atomic_exchange_1(volatile void* ptr,
+                                                        unsigned char val,
+                                                        int memorder)  // NOLINT
 {
   UNUSED(memorder);
-  volatile unsigned char *addr = (volatile unsigned char *)ptr;
+  volatile unsigned char* addr = (volatile unsigned char*)ptr;
   unsigned char old_val = 0;
 
   uint32_t primask = atomic_enter();
@@ -186,11 +195,11 @@ __atomic_exchange_1(volatile void *ptr, unsigned char val, int memorder) // NOLI
  * @param  memorder 内存顺序标志（忽略） / Memory order (ignored)
  * @retval 无返回值 / None
  */
-__attribute__((weak)) void 
-__atomic_store_1(volatile void *ptr, unsigned char val, int memorder) // NOLINT
+__attribute__((weak)) void __atomic_store_1(volatile void* ptr, unsigned char val,
+                                            int memorder)  // NOLINT
 {
   UNUSED(memorder);
-  volatile unsigned char *addr = (volatile unsigned char *)ptr;
+  volatile unsigned char* addr = (volatile unsigned char*)ptr;
 
   uint32_t primask = atomic_enter();
   *addr = val;
@@ -205,18 +214,17 @@ __atomic_store_1(volatile void *ptr, unsigned char val, int memorder) // NOLINT
  * @retval 返回之前的值 / Returns the previous value (0 or 1)
  */
 #if !defined(__clang__)
-__attribute__((weak)) _Bool
-__atomic_test_and_set(volatile void *ptr, int memorder)
+__attribute__((weak)) _Bool __atomic_test_and_set(volatile void* ptr, int memorder)
 {
   UNUSED(memorder);
-  volatile unsigned char *addr = (volatile unsigned char *)ptr;
+  volatile unsigned char* addr = (volatile unsigned char*)ptr;
   _Bool old_val = false;
 
   uint32_t primask = atomic_enter();
   old_val = *addr;
-  *addr = 1; 
+  *addr = 1;
   atomic_exit(primask);
-  
+
   return old_val;
 }
 #endif
