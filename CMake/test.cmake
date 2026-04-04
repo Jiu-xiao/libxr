4a78e24b8b6c0d35c7e9e43700f181a9cde418fa
Jiu-xiao <Jiu-xiao@users.noreply.github.com>
2026-04-04 12:50:44 +0100

    style(cmake): normalize first-party formatting and sort source globs

--- CMake/test.cmake
+++ CMake/test.cmake
@@ -1,28 +1,22 @@
 if(LIBXR_SYSTEM MATCHES "Linux")
-message("LibXR union test build.")
+  message("LibXR union test build.")
 else()
-message(FATAL_ERROR "Only linux can build union test.")
+  message(FATAL_ERROR "Only linux can build union test.")
 endif()
 
 project(test)
 
 add_compile_options(-g)
 
-file(
-  GLOB TEST_SOURCES "${CMAKE_CURRENT_SOURCE_DIR}/test/*.cpp")
-
+file(GLOB TEST_SOURCES "${CMAKE_CURRENT_SOURCE_DIR}/test/*.cpp")
 
 add_executable(test ${TEST_SOURCES})
 
 add_dependencies(test xr)
 
-target_link_libraries(
-    test
-    PUBLIC xr
-)
+target_link_libraries(test PUBLIC xr)
 
 target_include_directories(
-    test
-    PUBLIC $<TARGET_PROPERTY:xr,INTERFACE_INCLUDE_DIRECTORIES>
-    PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/lib/Eigen
-)
+  test
+  PUBLIC $<TARGET_PROPERTY:xr,INTERFACE_INCLUDE_DIRECTORIES>
+  PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/lib/Eigen)
