914cf7992db64c1fb220a6c11f4bb4d85c395866
CaFeZn <1837781998@qq.com>
2026-06-29 23:51:10 +0800

    test(hpm): drop GPIO IRQ regression from PR

--- test/hpm_gpio_irq_shared/stub_sdk/hpm_soc.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "hpm_test_state.hpp"
-
-#define HPM_GPIO0 (&LibXRHpmTest::g_gpio0)
-#define HPM_FGPIO (&LibXRHpmTest::g_fgpio)
-#define HPM_IOC (&LibXRHpmTest::g_ioc)
-#define HPM_PIOC (&LibXRHpmTest::g_pioc)
